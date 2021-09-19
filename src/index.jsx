daad27a30d9f3df445e72f45c24d7e40dd27edda
Mehedi Hasan Bijoy <mehedibubt34@gmail.com>
2021-09-19 23:17:26 +0600

    todo frontend

---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import {render} from "react-dom"
+
+import "./styles/main.scss"
+
+const Index = () => {
+    return (
+        <div>
+            <h1>
+                Mehedi Hasan Bappi
+            </h1>
+        </div>
+    )
+}
+
+render(<Index/>, document.getElementById('root'))
